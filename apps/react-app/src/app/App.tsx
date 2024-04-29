6d8d64414bb245e7add5b183e10b58279753763c
tanmayi-dev <135940047+tanmayi-dev@users.noreply.github.com>
2024-04-29 15:13:36 +0530

    upload react app

---
+++ apps/react-app/src/app/App.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Link, Route, Routes } from 'react-router-dom';
+
+export function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <Routes>
+        <Route path="/" element={<div>Route</div>} />
+        <Route path="*" element={<div>{}</div>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
